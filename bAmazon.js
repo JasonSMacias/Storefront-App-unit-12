b12033b08bf5c3cf54a017043a720ebbd329e735
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-01-31 17:38:13 -0500

    worked on inquirer

--- bAmazon.js
+++ bAmazon.js
@@ -7,6 +7,8 @@ const mysql = require("mysql");
 
 const cTable = require("console.table");
 
+let itemNumbers = [];
+
 // variable storing password from .env
 const localDBPW = process.env.MYSQL_PW
 
@@ -30,15 +32,44 @@ connection.connect(function (err) {
 });
 
 function startFunction() {
+
   connection.query("SELECT * FROM products", function (err, result) {
     if (err) throw (err);
 
     console.log("Available products\n");
-    for (var x in result){
+    for (var x in result) {
       console.log([
-        result[x].item_id, result[x].product_name, result[x].price]);
+        result[x].item_id, result[x].product_name, result[x].price
+      ]);
+      itemNumbers.push(result[x].item_id);
     };
-    
-    connection.end();
+
+  console.log(itemNumbers);
+  beginPrompt();
   });
+};
+function beginPrompt(){
+  inquirer.prompt([
+    {
+      name: "Choose item",
+      type: "input",
+      message: "Please select the item number of the item you would like to order",
+    },
+    {
+      name: "Choose another item",
+      type: "input",
+      message: "Please select another",
+    },
+  ])
+  .then(function(answer) {
+      // based on their answer, either call the bid or the post functions
+      if (answer.postOrBid === "POST") {
+        postAuction();
+      }
+      else if(answer.postOrBid === "BID") {
+        bidAuction();
+      } else{
+        connection.end();
+      }
+    });
 };
